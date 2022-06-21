5880d52b4397898a6c90dc1dc4d33f7be83b5ed3
Edgar Nevárez <edgar_neva@outlook.com>
2022-06-21 13:07:00 -0500

    .

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,10 @@ import { Link, Outlet } from "react-router-dom";
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../images/banana.png";
+import Axios from "axios";
+
 export default function navbar() {
+  if (localStorage.getItem("token" === null)) {
   return (
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -32,4 +35,35 @@ export default function navbar() {
       </section>
     </>
   );
+  }else{
+    return (
+      <>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container className="container">
+            <Navbar.Brand as={Link} to="/">
+              <img src={logo} alt="logo" width="50px" height="auto" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse
+              className="justify-content-end"
+              id="basic-navbar-nav"
+            >
+              <Nav>
+                <Nav.Link as={Link} to="/home">
+                  Logout
+                </Nav.Link>
+                <Nav.Link as={Link} to="/profile">
+                  Profile
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+  
+        <section>
+          <Outlet></Outlet>
+        </section>
+      </>
+    );
+}
 }
