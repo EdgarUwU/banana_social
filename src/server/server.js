7101766a377b8038666d1d4d2a5b299156a77a14
Edgar Nevárez <edgar_neva@outlook.com>
2022-06-20 23:04:14 -0500

    .

--- src/server/server.js
+++ src/server/server.js
@@ -4,12 +4,11 @@ const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Alapastrucka1#_",
-  database: "login"
+  database: "login",
 });
 
 app.use(cors());
@@ -17,8 +16,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
@@ -31,15 +30,40 @@ app.post("/login", (req, res) => {
         res.status(500).send("Error");
       } else {
         if (results.length > 0) {
-          res.send("Login Successful");
+          res.send(results);
         } else {
           res.send("Login Failed");
         }
       }
     }
   );
-}
-);
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  db.query(
+    "INSERT INTO users (username, contrasena) VALUES (?, ?)",
+    [username, password],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error");
+      } else {
+        res.send("User Registered");
+      }
+    }
+  );
+});
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error");
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
